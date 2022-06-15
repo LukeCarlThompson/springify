416b50992694e124cd5f28bcd1b708719c69b755
Luke Carl Thompson <lukecarlthompson@gmail.com>
2022-06-15 12:34:40 +0800

    move methods inside constructor

--- src/scripts/Springify.ts
+++ src/scripts/Springify.ts
@@ -9,10 +9,10 @@ mass: effective range from 0 - 100;
 const percentToValueBetweenRange = (percent: number, min: number, max: number) => (percent * (max - min)) / 100 + min;
 
 interface SpringifyProps {
+  input?: number;
   stiffness?: number;
   damping?: number;
   mass?: number;
-  input?: number;
   onFrame: (output: number, velocity: number) => void;
   onFinished?: () => void;
 }
@@ -33,23 +33,27 @@ export class Springify {
   velocity = 0;
   amplitude = 0;
   animating;
-  callback;
+  onFrame;
+  onFinished;
   lastTime;
   currentTime;
   delta;
   animationFrame;
-  onFinished;
+  interpolate;
+  animate;
+  animLoop;
 
   constructor({
+    input = 0,
     stiffness = 10,
     damping = 30,
     mass = 20,
-    input = 0,
-    onFrame = () => null,
+    onFrame = () => {},
     onFinished = () => {},
   }: SpringifyProps) {
     this.animating = false;
-    this.callback = onFrame;
+    this.onFrame = onFrame;
+    this.onFinished = onFinished;
     this.lastTime = 0;
     this.currentTime = 0;
     this.delta = 0;
@@ -57,54 +61,53 @@ export class Springify {
     this.stiffness = stiffness;
     this.damping = damping;
     this.mass = mass;
-    this.onFinished = onFinished;
     this._input = input;
 
     // Set the output to start from the input value for the first frame
     this.output = this.input;
+
+    // Takes in arg object and sets interpolated values on that object based on some spring physics equations
+    this.interpolate = () => {
+      const stiffness = percentToValueBetweenRange(this.stiffness, -1, -300);
+      const damping = percentToValueBetweenRange(this.damping, -0.4, -20);
+      const mass = percentToValueBetweenRange(this.mass, 0.1, 10);
+      const springX = stiffness * (this.output - this.input);
+      const damperX = damping * this.velocity;
+      this.amplitude = (springX + damperX) / mass;
+      this.velocity += this.amplitude * (this.delta / 1000);
+      this.output += this.velocity * (this.delta / 1000);
+    };
+
+    // The animation loop
+    this.animLoop = () => {
+      this.currentTime = Date.now();
+      if (!this.animating) this.lastTime = this.currentTime - 1;
+      this.delta = this.currentTime - this.lastTime;
+      this.lastTime = this.currentTime;
+
+      this.animating = true;
+      this.interpolate();
+
+      // execute the callback function and pass in our prop objects array containing the springified values
+      this.onFrame(this.output, this.velocity);
+
+      // returns true if each output value has reached the input
+      this.animating = !(Math.abs(this.velocity) < 0.2 && Math.abs(this.output - this.input) < 0.2);
+
+      // If not finished then cancel any queued frame and animate another frame
+      if (this.animating) {
+        cancelAnimationFrame(this.animationFrame);
+        this.animationFrame = requestAnimationFrame(this.animLoop);
+      } else {
+        this.animating = false;
+        this.onFinished();
+      }
+    };
+
+    this.animate = () => {
+      if (!this.animating) {
+        this.animLoop();
+      }
+    };
   }
-
-  // Takes in arg object and sets interpolated values on that object based on some spring physics equations
-  interpolate = () => {
-    const stiffness = percentToValueBetweenRange(this.stiffness, -1, -300);
-    const damping = percentToValueBetweenRange(this.damping, -0.4, -20);
-    const mass = percentToValueBetweenRange(this.mass, 0.1, 10);
-    const springX = stiffness * (this.output - this.input);
-    const damperX = damping * this.velocity;
-    this.amplitude = (springX + damperX) / mass;
-    this.velocity += this.amplitude * (this.delta / 1000);
-    this.output += this.velocity * (this.delta / 1000);
-  };
-
-  // The animation loop
-  animLoop = () => {
-    this.currentTime = Date.now();
-    if (!this.animating) this.lastTime = this.currentTime - 1;
-    this.delta = this.currentTime - this.lastTime;
-    this.lastTime = this.currentTime;
-
-    this.animating = true;
-    this.interpolate();
-
-    // execute the callback function and pass in our prop objects array containing the springified values
-    this.callback(this.output, this.velocity);
-
-    // returns true if each output value has reached the input
-    this.animating = !(Math.abs(this.velocity) < 0.2 && Math.abs(this.output - this.input) < 0.2);
-
-    // If not finished then cancel any queued frame and animate another frame
-    if (this.animating) {
-      cancelAnimationFrame(this.animationFrame);
-      this.animationFrame = requestAnimationFrame(this.animLoop);
-    } else {
-      this.animating = false;
-      this.onFinished();
-    }
-  };
-
-  animate = () => {
-    if (!this.animating) {
-      this.animLoop();
-    }
-  };
 }
