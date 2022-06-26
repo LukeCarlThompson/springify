c3008b3f3dd9ffd0b3f34c08c1024a7069bc9659
Luke Carl Thompson <lukecarlthompson@gmail.com>
2022-06-26 17:55:21 +0800

    improve docs and add new button demo

---
+++ src/scripts/buttonDemo.ts
@@ -0,0 +1,31 @@
+import { Springify } from './Springify';
+
+export default function () {
+  const button = document.querySelector('.demo-button') as HTMLButtonElement;
+
+  const springyButton = new Springify({
+    stiffness: 20,
+    damping: 30,
+    mass: 10,
+    input: 1,
+    onFrame: (output, velocity) => {
+      button.style.transform = `scaleX(${output + velocity * 0.1}) scaleY(${output})`;
+    },
+  });
+
+  button.addEventListener('mousedown', () => {
+    springyButton.input = 0.75;
+  });
+
+  button.addEventListener('mouseup', () => {
+    springyButton.input = 1.2;
+  });
+
+  button.addEventListener('mouseenter', () => {
+    springyButton.input = 1.2;
+  });
+
+  button.addEventListener('mouseout', () => {
+    springyButton.input = 1;
+  });
+}
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,11 +1,13 @@
 import 'prismjs';
 import '../styles/index.scss';
 import sailboatDemo from './sailboatDemo.js';
+import buttonDemo from './buttonDemo.js';
 import spiderDemo from './spiderDemo.js';
 import heliDemo from './heliDemo.js';
 
-heliDemo();
 sailboatDemo();
+buttonDemo();
 spiderDemo();
+heliDemo();
 
 // const sigmoid = x => x / (1 + Math.abs(x * 0.01));
