ca06e6b951d42c6f8bb991ca3d3485da04a67c2a
Luke Carl Thompson <lukecarlthompson@gmail.com>
2022-06-29 22:19:59 +0800

    improve docs and demos

--- src/scripts/buttonDemo.ts
+++ src/scripts/buttonDemo.ts
@@ -1,31 +1,42 @@
 import { Springify } from './Springify';
 
 export default function () {
-  const button = document.querySelector('.demo-button') as HTMLButtonElement;
+  const buttons = document.querySelectorAll('.demo-button') as NodeListOf<HTMLButtonElement>;
 
-  const springyButton = new Springify({
-    stiffness: 20,
-    damping: 30,
-    mass: 10,
-    input: 1,
-    onFrame: (output, velocity) => {
-      button.style.transform = `scaleX(${output + velocity * 0.1}) scaleY(${output})`;
-    },
-  });
+  buttons.forEach((button) => {
+    const springyButton = new Springify({
+      stiffness: 20,
+      damping: 30,
+      mass: 10,
+      input: 1,
+      onFrame: (output, velocity) => {
+        button.style.transform = `scaleX(${output + velocity * 0.1}) scaleY(${output})`;
+      },
+    });
 
-  button.addEventListener('mousedown', () => {
-    springyButton.input = 0.75;
-  });
+    button.addEventListener('mousedown', () => {
+      springyButton.input = 0.75;
+    });
 
-  button.addEventListener('mouseup', () => {
-    springyButton.input = 1.1;
-  });
+    button.addEventListener('touchstart', (e) => {
+      e.preventDefault();
+      springyButton.input = 0.75;
+    });
 
-  button.addEventListener('mouseenter', () => {
-    springyButton.input = 1.1;
-  });
+    button.addEventListener('mouseup', () => {
+      springyButton.input = 1.1;
+    });
 
-  button.addEventListener('mouseout', () => {
-    springyButton.input = 1;
+    button.addEventListener('mouseenter', () => {
+      springyButton.input = 1.1;
+    });
+
+    button.addEventListener('mouseout', () => {
+      springyButton.input = 1;
+    });
+
+    button.addEventListener('touchend', () => {
+      springyButton.input = 1;
+    });
   });
 }
