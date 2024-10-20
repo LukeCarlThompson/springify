446f63ead8bfa8aa29e55f0d29485ab2bee2e407
Luke Carl Thompson <lukecarlthompson@gmail.com>
2024-10-20 13:35:04 +0800

    update vite and tsconfigs

---
+++ vite.build-lib.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import eslint from 'vite-plugin-eslint';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  plugins: [
+    eslint(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+      exclude: ['./src/demos'],
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      name: 'springify',
+      fileName: '[name]',
+    },
+    rollupOptions: {
+      output: {
+        preserveModules: false,
+        inlineDynamicImports: false,
+      },
+      treeshake: 'smallest',
+    },
+  },
+});
